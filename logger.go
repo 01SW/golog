a0201cafd35ed8f0b018bd4fdc98e4d0fc88de7b
ws01 <01@ws01.ltd>
2020-11-28 16:39:34 +0800

    重写配置的方式

--- logger.go
+++ logger.go
@@ -12,22 +12,66 @@ import (
 
 var logger *zap.Logger = nil
 
-//获取初始配置数据
-func getDefaultJson()*[]byte{
-	defaultJson := []byte(`{
-    "level":"warn",
-    "encoding":"json",
-    "outputPaths": ["stdout", "log.txt"],
-    "errorOutputPaths": ["stderr"],
-    "encoderConfig": {
-      "messageKey": "message",
-	  "timeKey": "time",
-      "levelKey": "level",
-      "levelEncoder": "lowercase"
-    }
-  }`)
+//日志输出方式
+type LogWriteType int8
+const (
+	File LogWriteType = 0 //文件
+	Stdout LogWriteType = 1 //终端
+)
 
-	return &defaultJson
+//日志配置
+type logConfig struct{
+	WriteType LogWriteType //日志输出方式，默认为File
+	LevelEnum zapcore.Level //日志记录最低级别，默认为WarnLevel
+	Encoding string //日志编码方式，默认为json
+	SavePath string //日志保存路径，当WriteType为File时起效，默认为当前目录的log文件夹内的log.txt
+	FileMaxSize int //日志文件最大数量(单位MB)，默认为10MB
+	FileMaxBackups int //保留的旧日志文件最大数量， 默认为5个
+	FileMaxAge int //保留日志最大日期，默认为30天
+}
+
+//日志初始化工具类
+type News struct {
+}
+
+//获取初始配置数据
+func (ns *News)GetDefaultConfig()*logConfig {
+	defaultConfig := logConfig{}
+	defaultConfig.WriteType = File
+	defaultConfig.LevelEnum = zapcore.WarnLevel
+	defaultConfig.Encoding = "json"
+	defaultConfig.SavePath = "log/log.txt"
+	defaultConfig.FileMaxSize = 10
+	defaultConfig.FileMaxBackups = 5
+	defaultConfig.FileMaxAge = 30
+
+	return &defaultConfig
+}
+
+//获取指定了保存路径的配置数据(其余与初始配置一致)
+func (ns *News)GetConfig(path string)*logConfig{
+	config := ns.GetDefaultConfig()
+	config.SavePath = path
+	return config
+}
+
+//LogConfig转zap.Config
+func configChangeZap(config *logConfig)*zap.Config{
+	cfg := zap.Config{}
+
+	var outType []string
+	if config.WriteType == File{
+		outType = append(outType, config.SavePath)
+	}else{
+		outType = append(outType, "stdout")
+	}
+	cfg.OutputPaths = outType
+	cfg.Level = zap.NewAtomicLevelAt(config.LevelEnum)
+	cfg.Encoding = config.Encoding
+	cfg.EncoderConfig = zap.NewProductionEncoderConfig()
+	cfg.EncoderConfig.EncodeTime = timeEncoder
+
+	return &cfg
 }
 
 //时间格式
@@ -36,38 +80,23 @@ func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 }
 
 //日志分割
-func getLogWriter(path string)*lumberjack.Logger{
-	if path == ""{
-		return nil
-	}
-
+func getLogWriter(config *logConfig)*lumberjack.Logger{
 	lj := lumberjack.Logger{
-		Filename:   path, // 输出文件
-		MaxSize:    5,       // 日志文件最大大小（单位：MB）
-		MaxBackups: 3,         // 保留的旧日志文件最大数量
-		MaxAge:     30,        // 保存日期
+		Filename:   config.SavePath,           // 输出文件
+		MaxSize:    config.FileMaxSize,       // 日志文件最大大小（单位：MB）
+		MaxBackups: config.FileMaxBackups,    // 保留的旧日志文件最大数量
+		MaxAge:     config.FileMaxAge,        // 保存日期
 	}
 	return &lj
 }
 
-//获取Log初始配置
-func getConfig()*zap.Config{
-	var cfg zap.Config
-	er := json.Unmarshal(*getDefaultJson(), &cfg)
-	if er != nil{
-		panic("log初始化配置出错")
-	}
-	return &cfg
-}
-
-
 //初始化Log，需传递log配置文件路径，若该配置文件不存在则会以默认初始配置进行初始化
-func Init(path string){
+func (ns *News)Init(path string){
 	var ler error
-	var cfg *zap.Config
+	var config *logConfig
 	if _, er := os.Stat(path); er != nil{
-		cfg = getConfig()
-		cfg.EncoderConfig.EncodeTime = timeEncoder
+		config = ns.GetDefaultConfig()
+		cfg := configChangeZap(config)
 		logger, ler = cfg.Build()
 		if ler != nil{
 			panic("初始化日志失败，" + ler.Error())
@@ -77,7 +106,8 @@ func Init(path string){
 		if er != nil{
 			panic("无法创建配置文件，" + er.Error())
 		}
-		_, wer := file.Write(*getDefaultJson())
+		data, _ := json.MarshalIndent(config, "", " ")
+		_, wer := file.Write(data)
 		if wer != nil{
 			panic("写入配置文件失败，" + wer.Error())
 		}
@@ -92,12 +122,11 @@ func Init(path string){
 			panic("读取配置文件失败，" + fer.Error())
 		}
 
-		cfg = &zap.Config{}
-		cer := json.Unmarshal(data, cfg)
+		cer := json.Unmarshal(data, config)
 		if cer != nil{
 			panic("所读取的配置文件存在错误，" + cer.Error())
 		}
-		cfg.EncoderConfig.EncodeTime = timeEncoder
+		cfg := configChangeZap(config)
 		logger, ler = cfg.Build()
 
 		if ler != nil{
@@ -106,15 +135,46 @@ func Init(path string){
 	}
 
 	zap.ReplaceGlobals(logger)
-	out := cfg.OutputPaths
-	if len(out) == 2{
-		if out[0] == "stdout"{
-			zapcore.AddSync(getLogWriter(out[1]))
-		}else{
-			zapcore.AddSync(getLogWriter(out[0]))
-		}
-	}else if len(out) == 1 && out[0] != "stdout"{
-		zapcore.AddSync(getLogWriter(out[0]))
+	if config != nil && config.WriteType == File{
+		zapcore.AddSync(getLogWriter(config))
+	}
+}
+
+//以LogConfig结构体对象初始化日志
+func (ns *News)ConfigInit(config *logConfig){
+	cfg := configChangeZap(config)
+	var er error
+	logger, er = cfg.Build()
+	if er != nil{
+		panic("无法创建日志对象，" + er.Error())
+	}
+	zap.ReplaceGlobals(logger)
+	if config != nil && config.WriteType == File{
+		zapcore.AddSync(getLogWriter(config))
+	}
+}
+
+//将LogConfig结构体对象保存到本地
+func (ns *News)SaveConfig(path string, config *logConfig)error{
+	file, er := os.Create(path)
+	if er != nil{
+		return er
+	}
+	data, _ := json.MarshalIndent(config, "", " ")
+	_, wer := file.Write(data)
+	if wer != nil{
+		return wer
+	}
+	_ = file.Close()
+	return nil
+}
+
+//判断配置文件是否存在
+func (ns *News)ConfigExist(path string)bool{
+	if _, er := os.Stat(path); er != nil{
+		return false
+	}else{
+		return true
 	}
 }
 
